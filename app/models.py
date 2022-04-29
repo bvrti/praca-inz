ad7a2ce491e29bb531d0d0f6f896e75f3d891c1e
bvrti <74336093+bvrti@users.noreply.github.com>
2022-04-29 11:39:37 +0200

    custom permissions

--- app/models.py
+++ app/models.py
@@ -1,2 +1,13 @@
 from django.db import models
 # Create your models here.
+
+class Setting(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(editable=False)
+    description = models.TextField()
+    value = models.TextField()
+    class Meta:
+        #for permissions that don't really relate to a particular model, and I don't want to programmatically create them.
+        permissions = [
+            ("Operator", "Can report malfunction"),
+        ]
