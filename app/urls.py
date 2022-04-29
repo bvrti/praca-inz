504eaf1533360de7093aa11f663701da03c32079
bvrti <74336093+bvrti@users.noreply.github.com>
2022-04-29 13:35:12 +0200

    wymuszenie autoryzacji przed zgloszeniem awarii

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('login', views.index, name='login'),
+    path('report-malfunction/', views.reportMalfunction, name='reportMalfunction'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
     template = loader.get_template('app/index.html')
+    return HttpResponse(template.render({}, request))
+
+@login_required(login_url='/login/')
+def reportMalfunction(request):
+    template = loader.get_template('app/report-malfunction.html')
     return HttpResponse(template.render({}, request))
