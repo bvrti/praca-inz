c82d9f931000954876d7ef8d1983f65acde069ab
bvrti <74336093+bvrti@users.noreply.github.com>
2022-05-26 09:20:22 +0200

    azure debug

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views 
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('report-malfunction/', views.reportMalfunction, name='reportMalfunction'),
     path('test', views.test, name='test'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
